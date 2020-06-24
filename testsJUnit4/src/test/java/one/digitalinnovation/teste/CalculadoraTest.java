6a74abdd40a8b81bc38fce7a6af48730dc870cc5
Aluísio <aluisiomeloni@gmail.com>
2020-06-23 22:29:58 -0300

    Testando pequenos blocos com Mockito.

--- testsJUnit4/src/test/java/one/digitalinnovation/teste/CalculadoraTest.java
+++ testsJUnit4/src/test/java/one/digitalinnovation/teste/CalculadoraTest.java
@@ -3,6 +3,8 @@ package one.digitalinnovation.teste;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CalculadoraTest {
 
@@ -11,5 +13,16 @@ public class CalculadoraTest {
         Calculadora calculadora = new Calculadora();
         int soma = calculadora.somar("1+1+3");
         assertEquals(5, soma);
+    }
+
+    @Test
+    public void testeSomarComMock(){
+        Calculadora calculadora = mock(Calculadora.class);
+
+        when(calculadora.somar("1+2")).thenReturn(10);
+
+        int resultado = calculadora.somar("1+2");
+
+        assertEquals(10, resultado);
     }
 }
